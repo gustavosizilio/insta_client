48bf66b4c44a42732c4a40332974f73b19071d50
Gustavo Sizilio <gustavosizilio@gmail.com>
2018-12-21 15:08:43 -0300

    Refactorings and adding getUserPosts

--- insta.js
+++ insta.js
@@ -1,5 +1,6 @@
 let axios = require("axios");
 let md5 = require('md5');
+let _ = require('lodash');
 
 var insta = new function () {
     this.rhxGis = null;
@@ -7,7 +8,8 @@ var insta = new function () {
     this.queryURL = 'https://www.instagram.com/graphql/query/';
 
     this.query = {
-        getPostLikes : "e0f59e4a1c8d78d0161873bc2ee7ec44",
+        getPostLikes: "e0f59e4a1c8d78d0161873bc2ee7ec44",
+        getUserPosts: "66eb9403e44cc12e5b5ecda48b667d41",
         defaultPageSize: 50
     }
 
@@ -18,18 +20,18 @@ var insta = new function () {
     };
 
     //Used if the client can store the token
-    this.instance = function(rhxGis){
+    this.instance = function (rhxGis) {
         this.setRhxGis(rhxGis);
         return this;
     }
 
 
-    this.setRhxGis = function(rhxGis) {
-        this.rhxGis = rhxGis; 
+    this.setRhxGis = function (rhxGis) {
+        this.rhxGis = rhxGis;
     }
 
-    this.getRhxGis = function() {
-        if(this.rhxGis){
+    this.getRhxGis = function () {
+        if (this.rhxGis) {
             return new Promise((resolve, reject) => {
                 resolve(this.rhxGis);
             })
@@ -50,52 +52,67 @@ var insta = new function () {
         }
     }
 
-    this.makeRequest = function({ queryHash, queryVariables }) {
+    this.makeRequest = function ({ queryHash, queryVariables }) {
         return this.generateRequestSignature(queryVariables)
-        .then(signature => {
-            return axios.get(
-                `${this.queryURL}?query_hash=${queryHash}&variables=${JSON.stringify(queryVariables)}`,
-                {
-                    "headers": {
-                        'user-agent': "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36",
-                        'x-instagram-gis': `${signature}`
-                    },
-                })
-        });
+            .then(signature => {
+                return axios.get(
+                    `${this.queryURL}?query_hash=${queryHash}&variables=${JSON.stringify(queryVariables)}`,
+                    {
+                        "headers": {
+                            'user-agent': "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36",
+                            'x-instagram-gis': `${signature}`
+                        },
+                    })
+            });
     }
 
-    this.buildPagination = function({queryVariables, limit, end_cursor, data = []}){
+    this.buildPagination = function ({ queryVariables, limit, end_cursor, data = [] }) {
         let pagesize = this.query.defaultPageSize;
-        let remaining = limit - data.length;
-        if (remaining <= pagesize) {
-            pagesize = remaining;
+        let remaining;
+        let hasLimit = false;
+        if(limit){
+           hasLimit = true;
+           remaining = limit - data.length;
+           if (remaining <= pagesize) {
+               pagesize = remaining;
+           }
         }
+        
         queryVariables.first = pagesize;
         if (end_cursor) {
             queryVariables.after = end_cursor;
         }
-        return {remaining, pagesize}
+        
+        return { hasLimit, remaining, pagesize }
     }
 
+    this.defaultQuery = function ({ 
+        queryHash,
+        queryVariables,
+        edjeKey,
+        limit,
+        end_cursor,
+        data = []
+    }) {
 
-    this.getPostLikes = function({ post, limit, end_cursor, data = [] }) {
-        let queryHash = this.query.getPostLikes;
-        let queryVariables = { "shortcode": post, "include_reel": true };
         let pagination = this.buildPagination({
             queryVariables, limit, end_cursor, data
-        });        
-
+        });
+        
         return this.makeRequest({ queryHash, queryVariables }).then(res => {
-            if (res.data.data.shortcode_media && res.data.data.shortcode_media.edge_liked_by && res.data.data.shortcode_media.edge_liked_by.edges) {
-                data = data.concat(res.data.data.shortcode_media.edge_liked_by.edges);
+            let edge = _.get(res, edjeKey);
+            if (edge) {
+                data = data.concat(edge.edges);
                 if (
-                    res.data.data.shortcode_media.edge_liked_by.page_info.has_next_page &&
-                    pagination.remaining > pagination.pagesize
+                    edge.page_info.has_next_page &&
+                    (!pagination.hasLimit || pagination.remaining > pagination.pagesize)
                 ) {
-                    return this.getPostLikes({
-                        post: post,
+                    return this.defaultQuery({
+                        queryHash,
+                        queryVariables,
+                        edjeKey,
                         limit: limit,
-                        end_cursor: res.data.data.shortcode_media.edge_liked_by.page_info.end_cursor,
+                        end_cursor: edge.page_info.end_cursor,
                         data: data
                     })
                 } else {
@@ -107,49 +124,39 @@ var insta = new function () {
         })
     }
 
+    /**
+     * 
+     * 
+     * 
+     * 
+     * QUERY FUNCTIONS
+     * 
+     * 
+     * 
+     *
+     */
 
+    this.getPostLikes = function ({ identifier, limit, end_cursor, data = [] }) {
+        return this.defaultQuery({
+            queryHash: this.query.getPostLikes,
+            queryVariables: { "shortcode": identifier, "include_reel": true },
+            edjeKey: 'data.data.shortcode_media.edge_liked_by',
+            limit,
+            end_cursor,
+            data
+        })
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // getUserPosts = function getUserPosts({ user, end_cursor, data }) {
-    //     if (!data) {
-    //         data = [];
-    //     }
-    //     let queryVariables = { "id": user, "first": 50 };
-    //     if (end_cursor) {
-    //         queryVariables.after = end_cursor;
-    //     }
-    //     const queryHash = "66eb9403e44cc12e5b5ecda48b667d41"; //WHat to query?  
-
-    //     return makeRequest({ queryHash, queryVariables }).then(res => {
-    //         console.log(res.data.data);
-    //         // if(res.data.data.shortcode_media && res.data.data.shortcode_media.edge_liked_by && res.data.data.shortcode_media.edge_liked_by.edges) {
-    //         //     data = data.concat(res.data.data.shortcode_media.edge_liked_by.edges);
-    //         //     if(res.data.data.shortcode_media.edge_liked_by.page_info.has_next_page){
-    //         //         return getUserPosts({
-    //         //             user: user,
-    //         //             end_cursor: res.data.data.shortcode_media.edge_liked_by.page_info.end_cursor,
-    //         //             data: data
-    //         //         })
-    //         //     } else {
-    //         //         return data;
-    //         //     }
-    //         // } else {
-    //         //     return data;
-    //         // }
-    //     })
-    // }
+    this.getUserPosts = function ({ identifier, limit, end_cursor, data = [] }) {
+        return this.defaultQuery({
+            queryHash: this.query.getUserPosts,
+            queryVariables: { "id": identifier },
+            edjeKey: 'data.data.user.edge_owner_to_timeline_media',
+            limit,
+            end_cursor,
+            data
+        })
+    }
 
 }
 
--- sample.js
+++ sample.js
@@ -1,17 +1,12 @@
 let insta = new require("./insta").instance(); 
 
-insta.getPostLikes({post: "SQotORh6Gz", limit: 80}) //Pass the id of the post
+
+// insta.getPostLikes({identifier: "SQotORh6Gz", limit: 80}) //Pass the id of the post
+//     .then(data => {
+//         console.log(data); //Threat the data
+//     })
+
+insta.getUserPosts({identifier: "26669533"}) //Pass the id of the post
     .then(data => {
-        // console.log(data); //Threat the data
         console.log(data.length); //Threat the data
     })
-
-// insta.getAllPostLikes({post: "BrA7vWYHIsF"}) //Pass the id of the post
-//     .then(data => {
-//         console.log(data); //Threat the data
-//     })
-
-// insta.getPostLikes({user: "26669533"}) //Pass the id of the post
-//     .then(data => {
-//         console.log(data); //Threat the data
-//     })
