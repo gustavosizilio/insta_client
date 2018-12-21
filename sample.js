881e083bd0fb66d6d349b36ab4c566b21bf88406
Gustavo Sizilio <gustavosizilio@gmail.com>
2018-12-21 17:58:21 -0300

    Change npm name

--- sample.js
+++ sample.js
@@ -1,21 +1,21 @@
 let insta = new require("./insta").instance(); 
 
-insta.getUser({identifier: "gustavo.sizilio"}) //Pass the id of the post
+insta.getUser({identifier: "gustavo.sizilio"})
     .then(data => {
         console.log(data); //Threat the data
     })
 
-insta.getUserPosts({identifier: "26669533"}) //Pass the id of the post
-    .then(data => {
-        console.log(data.length); //Threat the data
-    })
-
-insta.getPostDetails({identifier: "SQotORh6Gz"}) //Pass the id of the post
+insta.getUserPosts({identifier: "26669533", limit: 10})
     .then(data => {
         console.log(data); //Threat the data
     })
 
-insta.getPostLikes({identifier: "SQotORh6Gz", limit: 80}) //Pass the id of the post
+insta.getPostDetails({identifier: "SQotORh6Gz"})
+    .then(data => {
+        console.log(data); //Threat the data
+    })
+
+insta.getPostLikes({identifier: "SQotORh6Gz", limit: 80})
     .then(data => {
         console.log(data); //Threat the data
     })
